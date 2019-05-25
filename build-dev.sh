c00741317bb010ed7884606dad2849baeb355d87
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-05-24 17:04:28 -0700

    Added: build-dev

---
+++ build-dev.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -x -e
+
+organization="rogerxman"
+buildnumber=${4-$(date -u +"%y%m%d%H%M")}
+imagename="$organization/kudulite:dev$buildnumber"
+
+docker build --no-cache -t "$imagename" kudu
+docker tag "$imagename" "$imagename"
+
+# Build and Run
+docker container rm -f kudulite
