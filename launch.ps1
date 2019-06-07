5b04d3e6390a0ba66b5ced5b948dc3d1635e65aa
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-06-07 15:19:27 -0700

    Added: launch.ps1

--- launch.ps1
+++ launch.ps1
@@ -1,7 +1,13 @@
-$old = (docker container ls -q --filter "name=kudulite")
+$old_id = (docker container ls -q --filter "name=kudulite")
+$old_image = (docker container ls --filter "name=kudulite" --format "{{.Image}}")
 
-if ($old) {
-    docker container rm -f $old
+if ($old_id) {
+    docker container rm -f $old_id
 }
 
-docker run --name kudulite --publish 8080:80 -e "CONTAINER_ENCRYPTION_KEY=MDEyMzQ1Njc4OUFCQ0RFRjAxMjM0NTY3ODlBQkNERUY=" -e "WEBSITE_AUTH_ENCRYPTION_KEY=E782F47415EEC076F8087729FB30CF1CA1482610C2FA985E4F22531225722205" -e "WEBSITE_SITE_NAME=azclidemo" -e "WEBSITE_HOSTNAME=azclidemo.azurewebsites.com" $args[0]
+$imagename = $args[0]
+if (-not $imagename) {
+    $imagename = $old_image
+}
+
+docker run --name kudulite --publish 8080:80 -e "CONTAINER_NAME=awesome_id" -e "CONTAINER_ENCRYPTION_KEY=MDEyMzQ1Njc4OUFCQ0RFRjAxMjM0NTY3ODlBQkNERUY=" -e "WEBSITE_AUTH_ENCRYPTION_KEY=E782F47415EEC076F8087729FB30CF1CA1482610C2FA985E4F22531225722205" -e "WEBSITE_SITE_NAME=azclidemo" -e "WEBSITE_HOSTNAME=azclidemo.azurewebsites.com" $imagename
