939e06f8f9f6aa90ff68658c928d2e298b000c18
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-07-31 13:46:18 -0700

    Update stage

--- launch.ps1
+++ launch.ps1
@@ -10,4 +10,4 @@ if (-not $imagename) {
     $imagename = $old_image
 }
 
-docker run --name kudulite --publish 8080:80 -e "CONTAINER_NAME=awesome_id" -e "CONTAINER_ENCRYPTION_KEY=MDEyMzQ1Njc4OUFCQ0RFRjAxMjM0NTY3ODlBQkNERUY=" -e "WEBSITE_AUTH_ENCRYPTION_KEY=E782F47415EEC076F8087729FB30CF1CA1482610C2FA985E4F22531225722205" -e "WEBSITE_SITE_NAME=azclidemo" -e "WEBSITE_HOSTNAME=azclidemo.azurewebsites.com" $imagename
+docker run -it --rm --privileged --name kudulite --publish 8080:80 -e "CONTAINER_NAME=awesome_id" -e "CONTAINER_ENCRYPTION_KEY=MDEyMzQ1Njc4OUFCQ0RFRjAxMjM0NTY3ODlBQkNERUY=" -e "WEBSITE_AUTH_ENCRYPTION_KEY=E782F47415EEC076F8087729FB30CF1CA1482610C2FA985E4F22531225722205" -e "WEBSITE_SITE_NAME=azclidemo" -e "WEBSITE_HOSTNAME=azclidemo.azurewebsites.com" $imagename
